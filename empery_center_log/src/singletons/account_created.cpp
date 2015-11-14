b1acb0cc8bc7194ef916e706e420797b00fe1a7f
lhmouse <lh_mouse@126.com>
2015-11-14 13:28:35 +0800

    updates.

---
+++ empery_center_log/src/singletons/account_created.cpp
@@ -0,0 +1,21 @@
+#include "../precompiled.hpp"
+#include <poseidon/singletons/event_dispatcher.hpp>
+#include "../mysql/account_created.hpp"
+#include "../../../empery_center/src/events/account.hpp"
+
+namespace EmperyCenterLog {
+
+using namespace EmperyCenter;
+
+MODULE_RAII_PRIORITY(handles, 5000){
+	auto listener = Poseidon::EventDispatcher::register_listener<Events::AccountCreated>(
+		[](const boost::shared_ptr<Events::AccountCreated> &event){
+			const auto obj = boost::make_shared<MySql::CenterLog_AccountCreated>(Poseidon::get_local_time(),
+				event->account_uuid.get(), event->remote_ip);
+			obj->async_save(true);
+		});
+	LOG_EMPERY_CENTER_LOG_DEBUG("Created AccountCreated listener");
+	handles.push(std::move(listener));
+}
+
+}
