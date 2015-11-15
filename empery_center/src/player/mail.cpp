7b39004beeb56cf8a44f1ae5d777abce709b2289
lhmouse <lh_mouse@126.com>
2015-11-15 23:36:27 +0800

    temp

---
+++ empery_center/src/player/mail.cpp
@@ -0,0 +1,68 @@
+#include "../precompiled.hpp"
+#include "common.hpp"
+#include "../msg/cs_mail.hpp"
+#include "../msg/sc_mail.hpp"
+#include "../msg/cerr_mail.hpp"
+#include "../singletons/mail_box_map.hpp"
+#include "../mail_box.hpp"
+
+namespace EmperyCenter {
+
+PLAYER_SERVLET(Msg::CS_MailGetAllMails, account_uuid, session, /* req */){
+	const auto mail_box = MailBoxMap::require(account_uuid);
+	mail_box->pump_status(true);
+
+	return Response();
+}
+/*
+PLAYER_SERVLET(Msg::CS_ItemTradeFromRecharge, account_uuid, session, req){
+	const auto item_box = ItemBoxMap::require(account_uuid);
+	item_box->pump_status(true);
+
+	const auto repeat_count = req.repeat_count;
+	if(repeat_count == 0){
+		return Response(Msg::CERR_ZERO_REPEAT_TIMES);
+	}
+	const auto recharge_id = RechargeId(req.recharge_id);
+	const auto recharge_data = Data::ItemRecharge::get(recharge_id);
+	if(!recharge_data){
+		return Response(Msg::CERR_NO_SUCH_RECHARGE_ID) <<recharge_id;
+	}
+	const auto trade_data = Data::ItemTrade::require(recharge_data->trade_id);
+
+	std::vector<ItemTransactionElement> transaction;
+	Data::ItemTrade::unpack(transaction, trade_data, repeat_count, req.ID);
+	const auto insuff_item_id = item_box->commit_transaction_nothrow(transaction.data(), transaction.size());
+	if(insuff_item_id){
+		return Response(Msg::CERR_NO_ENOUGH_ITEMS) <<insuff_item_id;
+	}
+
+	return Response();
+}
+
+PLAYER_SERVLET(Msg::CS_ItemTradeFromShop, account_uuid, session, req){
+	const auto item_box = ItemBoxMap::require(account_uuid);
+	item_box->pump_status(true);
+
+	const auto repeat_count = req.repeat_count;
+	if(repeat_count == 0){
+		return Response(Msg::CERR_ZERO_REPEAT_TIMES);
+	}
+	const auto shop_id = ShopId(req.shop_id);
+	const auto shop_data = Data::ItemShop::get(shop_id);
+	if(!shop_data){
+		return Response(Msg::CERR_NO_SUCH_SHOP_ID) <<shop_id;
+	}
+	const auto trade_data = Data::ItemTrade::require(shop_data->trade_id);
+
+	std::vector<ItemTransactionElement> transaction;
+	Data::ItemTrade::unpack(transaction, trade_data, repeat_count, req.ID);
+	const auto insuff_item_id = item_box->commit_transaction_nothrow(transaction.data(), transaction.size());
+	if(insuff_item_id){
+		return Response(Msg::CERR_NO_ENOUGH_ITEMS) <<insuff_item_id;
+	}
+
+	return Response();
+}
+*/
+}
