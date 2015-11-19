c05b892d1f1e639804642e4ffd6f3d1b0211892d
lhmouse <lh_mouse@126.com>
2015-11-19 21:49:12 +0800

    bug fix.

--- empery_gold_scramble/src/utilities.cpp
+++ empery_gold_scramble/src/utilities.cpp
@@ -234,10 +234,16 @@ namespace {
 		if(dt.hr < game_start_o_clock){
 			// 设为当天开始时间。
 			dt.hr = game_start_o_clock;
+			dt.min = 0;
+			dt.sec = 0;
+			dt.ms = 0;
 			next_game_begin_time = Poseidon::assemble_time(dt);
 		} else if(dt.hr > game_end_o_clock){
 			// 设为第二天开始时间。
 			dt.hr = game_start_o_clock;
+			dt.min = 0;
+			dt.sec = 0;
+			dt.ms = 0;
 			next_game_begin_time = Poseidon::assemble_time(dt) + 86400 * 1000;
 		} else {
 			next_game_begin_time = utc_now + next_game_delay;
