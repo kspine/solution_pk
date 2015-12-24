b1f3af6fdad1208dafbf66eee2f593d7c075378e
lhmouse <lh_mouse@126.com>
2015-12-24 14:32:12 +0800

    updates.

--- empery_center/src/item_ids.hpp
+++ empery_center/src/item_ids.hpp
@@ -8,8 +8,10 @@ namespace EmperyCenter {
 namespace ItemIds {
 
 constexpr ItemId
-	ID_GOLD                 ( 1103001 ),
+	ID_GOLD_COINS           ( 1103001 ),
+	ID_SPRING               ( 1103002 ),
 	ID_DIAMONDS             ( 1103003 ),
+	ID_GOLD                 ( 1103004 ),
 
 	ID_SIGNING_IN           ( 2100020 ),
 	ID_LAND_UPGRADE_TICKET  ( 2100032 ),
