f4d94ad4c79ee6bb59848592a463da04851b5a0c
lhmouse <lh_mouse@126.com>
2015-12-29 14:25:56 +0800

    updates.

--- empery_center/src/account/promotion.cpp
+++ empery_center/src/account/promotion.cpp
@@ -148,7 +148,9 @@ namespace {
 		g_level_config = std::move(level_config);
 	}
 
-	Poseidon::JsonObject get_json_from_promotion_server(const char *name, Poseidon::OptionalMap get_params){
+	int check_login_backtrace(boost::shared_ptr<Account> &new_account,
+		const std::string &login_name, const std::string &password, const std::string &ip)
+	{
 		PROFILE_ME;
 
 		const auto sock_addr = boost::make_shared<Poseidon::SockAddr>();
@@ -164,19 +166,79 @@ namespace {
 			client = boost::make_shared<SimpleHttpClient>(*sock_addr, g_server_use_ssl, promise);
 			client->go_resident();
 
+			Poseidon::OptionalMap get_params;
+			get_params.set(sslit("loginName"), login_name);
+			get_params.set(sslit("password"),  password);
+			get_params.set(sslit("ip"),        ip);
+
 			Poseidon::OptionalMap headers;
 			headers.set(sslit("Host"), g_server_host);
 			headers.set(sslit("Connection"), "Close");
 			if(!g_server_auth.empty()){
 				headers.set(sslit("Authorization"), "Basic " + Poseidon::Http::base64_encode(g_server_auth));
 			}
-			client->send(g_server_path + "/" + name, std::move(get_params), std::move(headers));
+			client->send(g_server_path + "/checkLoginBacktrace", std::move(get_params), std::move(headers));
 			Poseidon::JobDispatcher::yield(promise);
 			promise->check_and_rethrow();
 		}
-
 		std::istringstream iss(client->get_entity().dump());
-		return Poseidon::JsonParser::parse_object(iss);
+		auto response_object = Poseidon::JsonParser::parse_object(iss);
+		LOG_EMPERY_CENTER_DEBUG("Promotion server response: ", response_object.dump());
+		auto error_code = static_cast<int>(response_object.at(sslit("errorCode")).get<double>());
+		if(error_code != Msg::ST_OK){
+			switch(error_code){
+			case EmperyPromotion::Msg::ERR_NO_SUCH_ACCOUNT:
+				error_code = Msg::ERR_NO_SUCH_ACCOUNT;
+				break;
+			case EmperyPromotion::Msg::ERR_INVALID_PASSWORD:
+				error_code = Msg::ERR_INVALID_PASSWORD;
+				break;
+			case EmperyPromotion::Msg::ERR_ACCOUNT_BANNED:
+				error_code = Msg::ERR_ACCOUNT_BANNED;
+				break;
+			default:
+				error_code = Msg::ERR_NO_SUCH_ACCOUNT; // XXX
+				break;
+			}
+		}
+		const auto referrers_it = response_object.find(sslit("referrers"));
+		if(referrers_it != response_object.end()){
+			const auto &referrers_array = referrers_it->second.get<Poseidon::JsonArray>();
+
+			AccountUuid referrer_uuid;
+
+			const auto create_or_update_account = [&](const std::string &cur_login_name, unsigned cur_level){
+				LOG_EMPERY_CENTER_DEBUG("Create or update account: cur_login_name = ", cur_login_name, ", cur_level = ", cur_level);
+				auto account = AccountMap::get_by_login_name(PROMOTION_PLATFORM_ID, cur_login_name);
+				if(!account){
+					const auto account_uuid = AccountUuid(Poseidon::Uuid::random());
+					const auto utc_now = Poseidon::get_utc_time();
+					LOG_EMPERY_CENTER_INFO("Creating new account: account_uuid = ", account_uuid, ", cur_login_name = ", cur_login_name);
+					account = boost::make_shared<Account>(account_uuid, PROMOTION_PLATFORM_ID, cur_login_name,
+						referrer_uuid, cur_level, utc_now, cur_login_name);
+					AccountMap::insert(account, ip); // XXX use real ip
+				} else {
+					account->set_promotion_level(cur_level);
+				}
+				referrer_uuid = account->get_account_uuid();
+				return account;
+			};
+
+			for(auto it = referrers_array.rbegin(); it != referrers_array.rend(); ++it){
+				auto &elem = it->get<Poseidon::JsonObject>();
+
+				const auto &referrer_login_name = elem.at(sslit("loginName")).get<std::string>();
+				const auto &referrer_level_str = elem.at(sslit("level")).get<std::string>();
+				const unsigned referrer_level = g_level_config.at(referrer_level_str);
+
+				create_or_update_account(referrer_login_name, referrer_level);
+			}
+
+			const auto level_str = response_object.at(sslit("level")).get<std::string>();
+			const unsigned level = g_level_config.at(level_str);
+			new_account = create_or_update_account(login_name, level);
+		}
+		return error_code;
 	}
 }
 
@@ -185,66 +247,15 @@ ACCOUNT_SERVLET("promotion/check_login", root, session, params){
 	const auto &password   = params.at("password");
 	const auto &token      = params.at("token");
 
-	Poseidon::OptionalMap get_params;
-	get_params.set(sslit("loginName"), login_name);
-	get_params.set(sslit("password"),  password);
-	get_params.set(sslit("ip"),        session->get_remote_info().ip.get());
-	const auto promotion_response_object = get_json_from_promotion_server("checkLoginBacktrace", std::move(get_params));
-	LOG_EMPERY_CENTER_DEBUG("Promotion server response: ", promotion_response_object.dump());
-	const auto error_code = static_cast<int>(promotion_response_object.at(sslit("errorCode")).get<double>());
-	if(error_code != Msg::ST_OK){
-		switch(error_code){
-		case EmperyPromotion::Msg::ERR_NO_SUCH_ACCOUNT:
-			return Response(Msg::ERR_NO_SUCH_ACCOUNT) <<login_name;
-		case EmperyPromotion::Msg::ERR_INVALID_PASSWORD:
-			return Response(Msg::ERR_INVALID_PASSWORD) <<login_name;
-		case EmperyPromotion::Msg::ERR_ACCOUNT_BANNED:
-			return Response(Msg::ERR_ACCOUNT_BANNED) <<login_name;
-		}
-		return Response(Msg::ERR_NO_SUCH_ACCOUNT) <<login_name; // XXX
-	};
-
-	const auto utc_now = Poseidon::get_utc_time();
-
-	std::deque<std::pair<std::string, unsigned>> accounts;
-	{
-		const auto level_str = promotion_response_object.at(sslit("level")).get<std::string>();
-		const unsigned level = g_level_config.at(level_str);
-		accounts.emplace_back(login_name, level);
-
-		const auto &referrers_array = promotion_response_object.at(sslit("referrers")).get<Poseidon::JsonArray>();
-		for(auto it = referrers_array.begin(); it != referrers_array.end(); ++it){
-			auto &elem = it->get<Poseidon::JsonObject>();
-
-			auto &referrer_login_name = elem.at(sslit("loginName")).get<std::string>();
-			const auto &referrer_level_str = elem.at(sslit("level")).get<std::string>();
-			const unsigned referrer_level = g_level_config.at(referrer_level_str);
-			accounts.emplace_back(std::move(referrer_login_name), referrer_level);
-		}
-	}
-
 	boost::shared_ptr<Account> account;
-	AccountUuid referrer_uuid;
-	{
-		for(auto it = accounts.rbegin(); it != accounts.rend(); ++it){
-			const auto &cur_login_name = it->first;
-			const auto cur_level = it->second;
-			LOG_EMPERY_CENTER_DEBUG("Updating account: cur_login_name = ", cur_login_name, ", cur_level = ", cur_level);
-			account = AccountMap::get_by_login_name(PROMOTION_PLATFORM_ID, cur_login_name);
-			if(!account){
-				const auto account_uuid = AccountUuid(Poseidon::Uuid::random());
-				LOG_EMPERY_CENTER_INFO("Creating new account: account_uuid = ", account_uuid, ", cur_login_name = ", cur_login_name);
-				account = boost::make_shared<Account>(account_uuid, PROMOTION_PLATFORM_ID, cur_login_name,
-					referrer_uuid, cur_level, utc_now, cur_login_name);
-				AccountMap::insert(account, session->get_remote_info().ip.get()); // XXX use real ip
-			} else {
-				account->set_promotion_level(cur_level);
-			}
-			referrer_uuid = account->get_account_uuid();
-		}
+	const auto error_code = check_login_backtrace(account, login_name, password, session->get_remote_info().ip.get());
+	if(error_code != Msg::ST_OK){
+		return Response(error_code) <<login_name;
 	}
-	assert(account);
-
+	if(!account){
+		DEBUG_THROW(Exception, sslit("Null account pointer"));
+	}
+	const auto utc_now = Poseidon::get_utc_time();
 	if(utc_now < account->get_banned_until()){
 		return Response(Msg::ERR_ACCOUNT_BANNED) <<login_name;
 	}
@@ -264,13 +275,13 @@ ACCOUNT_SERVLET("promotion/renewal_token", root, session, params){
 	const auto &old_token  = params.at("oldToken");
 	const auto &token      = params.at("token");
 
-	const auto account = AccountMap::get_by_login_name(PROMOTION_PLATFORM_ID, login_name);
-	if(!account){
-		return Response(Msg::ERR_NO_SUCH_LOGIN_NAME) <<login_name;
+	boost::shared_ptr<Account> account;
+	const auto error_code = check_login_backtrace(account, login_name, std::string(), session->get_remote_info().ip.get());
+	if((error_code != Msg::ST_OK) && (error_code != Msg::ERR_INVALID_PASSWORD)){
+		return Response(error_code) <<login_name;
 	}
-	const auto utc_now = Poseidon::get_utc_time();
-	if(utc_now < account->get_banned_until()){
-		return Response(Msg::ERR_ACCOUNT_BANNED) <<login_name;
+	if(!account){
+		DEBUG_THROW(Exception, sslit("Null account pointer"));
 	}
 	if(old_token.empty()){
 		LOG_EMPERY_CENTER_DEBUG("Empty token");
@@ -279,6 +290,10 @@ ACCOUNT_SERVLET("promotion/renewal_token", root, session, params){
 	if(old_token != account->get_login_token()){
 		LOG_EMPERY_CENTER_DEBUG("Invalid token: expecting ", account->get_login_token(), ", got ", old_token);
 		return Response(Msg::ERR_INVALID_TOKEN) <<login_name;
+	}
+	const auto utc_now = Poseidon::get_utc_time();
+	if(utc_now < account->get_banned_until()){
+		return Response(Msg::ERR_ACCOUNT_BANNED) <<login_name;
 	}
 
 	const auto token_expiry_duration = get_config<std::uint64_t>("account_token_expiry_duration", 0);
