13cc8a588d76950dee0c047a5ea048a5d19211a4
lhmouse <lh_mouse@126.com>
2015-12-30 21:28:58 +0800

    updates.

--- empery_center/src/admin/account.cpp
+++ empery_center/src/admin/account.cpp
@@ -9,6 +9,62 @@
 
 namespace EmperyCenter {
 
+namespace {
+	void fill_account_object(Poseidon::JsonObject &object, const boost::shared_ptr<Account> &account){
+		PROFILE_ME;
+
+		object[sslit("account_uuid")]      = account->get_account_uuid().str();
+		object[sslit("platform_id")]       = account->get_platform_id().get();
+		object[sslit("login_name")]        = account->get_login_name();
+
+		object[sslit("referrer_uuid")]     = account->get_referrer_uuid().str();
+		object[sslit("promotion_level")]   = account->get_promotion_level();
+		object[sslit("nick")]              = account->get_nick();
+		object[sslit("activated")]         = account->has_been_activated();
+		object[sslit("banned_until")]      = account->get_banned_until();
+
+		Poseidon::JsonObject attribute_object;
+		boost::container::flat_map<AccountAttributeId, std::string> attributes;
+		account->get_attributes(attributes);
+		for(auto it = attributes.begin(); it != attributes.end(); ++it){
+			char str[64];
+			unsigned len = (unsigned)std::sprintf(str, "%lu", (unsigned long)it->first.get());
+			attribute_object[SharedNts(str, len)] = std::move(it->second);
+		}
+	}
+}
+
+ADMIN_SERVLET("account/get", root, session, params){
+	const auto account_uuid = AccountUuid(params.at("account_uuid"));
+
+	const auto account = AccountMap::get(account_uuid);
+	if(!account){
+		return Response(Msg::ERR_NO_SUCH_ACCOUNT) <<account_uuid;
+	}
+
+	Poseidon::JsonObject object;
+	fill_account_object(object, account);
+	root[sslit("account")] = std::move(object);
+
+	return Response();
+}
+
+ADMIN_SERVLET("account/get_by_login_name", root, session, params){
+	const auto platform_id = boost::lexical_cast<PlatformId>(params.at("platform_id"));
+	const auto &login_name = params.at("login_name");
+
+	const auto account = AccountMap::get_by_login_name(platform_id, login_name);
+	if(!account){
+		return Response(Msg::ERR_NO_SUCH_LOGIN_NAME) <<login_name;
+	}
+
+	Poseidon::JsonObject object;
+	fill_account_object(object, account);
+	root[sslit("account")] = std::move(object);
+
+	return Response();
+}
+
 ADMIN_SERVLET("account/add", root, session, params){
 	const auto platform_id = boost::lexical_cast<PlatformId>(params.at("platform_id"));
 	const auto &login_name = params.at("login_name");
