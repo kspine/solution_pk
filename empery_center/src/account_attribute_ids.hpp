266dc856c8972f9fe214a70d93c2365083b55a81
lhmouse <lh_mouse@126.com>
2016-01-06 13:46:27 +0800

    updates.

--- empery_center/src/account_attribute_ids.hpp
+++ empery_center/src/account_attribute_ids.hpp
@@ -17,10 +17,13 @@ constexpr AccountAttributeId
 	ID_GENDER                         (   1 ),
 	ID_AVATAR                         (   2 ),
 
-	ID_LAST_LOGGED_IN_TIME            ( 300 ),
-	ID_LAST_LOGGED_OUT_TIME           ( 301 ),
+	ID_LAST_LOGGED_IN_TIME            ( 200 ),
+	ID_LAST_LOGGED_OUT_TIME           ( 201 ),
+
+	//
 	ID_LAST_SIGNED_IN_TIME            ( 302 ),
 	ID_SEQUENTIAL_SIGNED_IN_DAYS      ( 303 ),
+	ID_AUCTION_CENTER_AVAILABLE       ( 304 ),
 
 	ID_LAST_CHAT_TIME_ADJACENT        ( 400 ),
 	ID_LAST_CHAT_TIME_TRADE           ( 401 ),
