a8ba8f8e576e5efb10670aad7a7e29a6d392c423
lhmouse <lh_mouse@126.com>
2016-01-10 18:46:50 +0800

    minor.

--- empery_center/src/mysql/auction.hpp
+++ empery_center/src/mysql/auction.hpp
@@ -27,11 +27,11 @@ namespace MySql {
 #define MYSQL_OBJECT_FIELDS \
 	FIELD_UUID              (account_uuid)	\
 	FIELD_UUID              (map_object_uuid)	\
+	FIELD_INTEGER_UNSIGNED  (item_id)	\
+	FIELD_BIGINT_UNSIGNED   (item_count)	\
 	FIELD_INTEGER_UNSIGNED  (resource_id)	\
 	FIELD_BIGINT_UNSIGNED   (resource_amount_locked)	\
-	FIELD_BIGINT_UNSIGNED   (resource_amount_fee)	\
-	FIELD_INTEGER_UNSIGNED  (item_id)	\
-	FIELD_BIGINT_UNSIGNED   (item_count)
+	FIELD_BIGINT_UNSIGNED   (resource_amount_fee)
 #include <poseidon/mysql/object_generator.hpp>
 
 }
