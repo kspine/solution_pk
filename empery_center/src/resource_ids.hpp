f980d365bc2529141ba2d08fd6e3abbfdb722f40
lhmouse <lh_mouse@126.com>
2016-01-10 12:43:46 +0800

    .

---
+++ empery_center/src/resource_ids.hpp
@@ -0,0 +1,25 @@
+#ifndef EMPERY_CENTER_RESOURCE_IDS_HPP_
+#define EMPERY_CENTER_RESOURCE_IDS_HPP_
+
+#include "id_types.hpp"
+
+namespace EmperyCenter {
+
+namespace ResourceIds {
+
+constexpr ResourceId
+	ID_GRAIN                ( 1101001 ),
+	ID_WOOD                 ( 1101002 ),
+	ID_STONE                ( 1101003 ),
+	ID_POPULATION           ( 1101004 ),
+
+	ID_GRAIN_LOCKED         ( 1101101 ),
+	ID_WOOD_LOCKED          ( 1101102 ),
+	ID_STONE_LOCKED         ( 1101103 ),
+	ID_POPULATION_LOCKED    ( 1101104 );
+
+}
+
+}
+
+#endif
