84c1e55be755ecb6b15f15b7e17422020facbd2d
lhmouse <lh_mouse@126.com>
2016-01-14 23:10:07 +0800

    updates.

--- update_data.sh
+++ update_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 prefix=../svn/paike_Numerical/configs/csvs
+destination=./etc/poseidon/empery_center_data
 
-(cd $prefix && svn up)
-find $prefix/ -name '*.csv' | xargs -i sh -c 'echo {} && iconv -f GB18030 -t UTF-8 {} -o ./etc/poseidon/empery_center_data/$(basename {})'
+(cd "$prefix" && svn up)
+find "$prefix" -name '*.csv' | xargs -i sh -c 'echo {} && iconv -f GB18030 -t UTF-8 {} -o '"$destination"'/$(basename {})'
+sed -i 's/\r$//' $(find "$destination" -name '*.csv')
