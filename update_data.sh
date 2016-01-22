f8794d49cc4534aed8b0c20f43d42ca0518f4e8e
lhmouse <lh_mouse@126.com>
2016-01-22 15:38:01 +0800

    updates.

--- update_data.sh
+++ update_data.sh
@@ -2,15 +2,9 @@
 
 set -e
 
-if [ -z "$1" ];
-then
-	echo Please specify a version.
-	exit 1
-fi
-
-prefix="../svn/paike_Numerical/$1"
+prefix="../svn/paike_Numerical"
 destination="./etc/poseidon/empery_center_data"
 
 (cd "$prefix" && svn up)
-find "$prefix" -name '*.csv' | xargs -i sh -c 'echo {} && iconv -f GB18030 -t UTF-8 {} -o '"$destination"'/$(basename {})'
+find "$prefix/configs_1.0.22" -name '*.csv' | xargs -i sh -c 'echo {} && iconv -f GB18030 -t UTF-8 {} -o '"$destination"'/$(basename {})'
 sed -i 's/\r$//' $(find "$destination" -name '*.csv')
