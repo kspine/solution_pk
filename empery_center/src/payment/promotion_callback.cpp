36f3e40da90fb7aa6f3dece194d1a4f24e668272
lhmouse <lh_mouse@126.com>
2016-02-25 11:24:28 +0800

    .

--- empery_center/src/payment/promotion_callback.cpp
+++ empery_center/src/payment/promotion_callback.cpp
@@ -56,10 +56,6 @@ PAYMENT_SERVLET("promotion_callback", root, session, params){
 		return Response(Msg::ERR_PAYMENT_TRANSACTION_COMMITTED);
 	}
 
-	const auto account = AccountMap::require(payment_transaction->get_account_uuid());
-	const auto item_box = ItemBoxMap::require(payment_transaction->get_account_uuid());
-	const auto mail_box = MailBoxMap::require(payment_transaction->get_account_uuid());
-
 	payment_transaction->commit(item_box, mail_box, remarks);
 
 	Poseidon::async_raise_event(
