5c973abec2729d0e2e4a048a5862046ae7881915
lhmouse <lh_mouse@126.com>
2016-02-26 14:14:38 +0800

    data

--- update_data.sh
+++ update_data.sh
@@ -7,4 +7,4 @@ destination="./etc/poseidon/empery_center_data"
 
 (cd "$prefix" && svn up)
 find "$prefix/configs_1.0" -name '*.csv' | xargs -i sh -c 'echo {} && iconv -f GB18030 -t UTF-8 {} -o '"$destination"'/$(basename {})'
-sed -i 's/\r$//' $(find "$destination" -name '*.csv')
+sed -i 's/^M$//' $(find "$destination" -name '*.csv')
