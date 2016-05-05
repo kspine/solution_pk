95b7f8e386e6007008e29ff0ad28e3f719f8ff24
lhmouse <lh_mouse@126.com>
2016-05-05 15:40:05 +0800

    data.

---
+++ empery_center/src/buff_ids.hpp
@@ -0,0 +1,17 @@
+#ifndef EMPERY_CENTER_BUFF_IDS_HPP_
+#define EMPERY_CENTER_BUFF_IDS_HPP_
+
+#include "id_types.hpp"
+
+namespace EmperyCenter {
+
+namespace BuffIds {
+
+constexpr BuffId
+	ID                                  ( 1       );
+
+}
+
+}
+
+#endif
