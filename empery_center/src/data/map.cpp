bfed2f2bb2d356c9a9d1ccb34232c88b2e3d7005
lhmouse <lh_mouse@126.com>
2016-05-14 13:17:57 +0800

    .

--- empery_center/src/data/map.cpp
+++ empery_center/src/data/map.cpp
@@ -133,7 +133,7 @@ namespace {
 			csv.get(elem.production_rate_modifier, "output_multiple");
 			csv.get(elem.capacity_modifier,        "resource_multiple");
 			csv.get(elem.soldiers_max,             "territory_hp");
-			csv.get(elem.self_healing_rate,        "recover_hp");
+			csv.get(elem.self_healing_rate,        "recovery_hp");
 
 			if(!ticket_map->insert(std::move(elem)).second){
 				LOG_EMPERY_CENTER_ERROR("Duplicate MapCellTicket: ticket_item_id = ", elem.ticket_item_id);
