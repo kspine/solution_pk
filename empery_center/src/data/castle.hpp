8a9e9555e1c249c6499c92017a7e7203669552d2
lhmouse <lh_mouse@126.com>
2016-05-18 11:13:09 +0800

    castle protection.

--- empery_center/src/data/castle.hpp
+++ empery_center/src/data/castle.hpp
@@ -77,7 +77,7 @@ namespace Data {
 		std::uint64_t max_map_cell_distance;
 		std::uint64_t max_immigrant_group_count;
 		std::uint64_t max_defense_buildings;
-		boost::container::flat_map<ItemId, std::uint64_t> protection_cost;
+		boost::container::flat_map<ResourceId, std::uint64_t> protection_cost;
 		std::uint64_t max_occupied_map_cells;
 	};
 
