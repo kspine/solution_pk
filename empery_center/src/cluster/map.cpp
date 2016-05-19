f6d99118ff6bf4a77549c4642b9b5805f5c1b620
lhmouse <lh_mouse@126.com>
2016-05-19 14:12:52 +0800

    Bug fix.

--- empery_center/src/cluster/map.cpp
+++ empery_center/src/cluster/map.cpp
@@ -919,8 +919,15 @@ CLUSTER_SERVLET(Msg::KS_MapAttackMapCellAction, cluster, req){
 	const auto attacked_ticket_item_id = attacked_cell->get_ticket_item_id();
 	const auto attacked_account_uuid = attacked_cell->get_owner_uuid();
 
-	if(attacking_account_uuid == attacked_account_uuid){
-		return Response(Msg::ERR_CANNOT_ATTACK_FRIENDLY_OBJECTS);
+	const auto occupier_owner_uuid = attacked_cell->get_occupier_owner_uuid();
+	if(occupier_owner_uuid){
+		if(attacking_account_uuid == occupier_owner_uuid){
+			return Response(Msg::ERR_CANNOT_ATTACK_FRIENDLY_OBJECTS);
+		}
+	} else {
+		if(attacking_account_uuid == attacked_account_uuid){
+			return Response(Msg::ERR_CANNOT_ATTACK_FRIENDLY_OBJECTS);
+		}
 	}
 
 	update_attributes_single(attacking_object, [&]{ return attacking_object_type_id != MapObjectTypeIds::ID_CASTLE; });
