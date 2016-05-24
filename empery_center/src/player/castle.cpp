9d11f67f13c602515a7fe38af392ce6c0172fc36
lhmouse <lh_mouse@126.com>
2016-05-24 18:35:30 +0800

    .

--- empery_center/src/player/castle.cpp
+++ empery_center/src/player/castle.cpp
@@ -1594,6 +1594,7 @@ PLAYER_SERVLET(Msg::CS_CastleRelocate, account, session, req){
 			modifiers[AttributeIds::ID_CASTLE_LAST_COORD_Y] = castle->get_coord().y();
 			castle->set_attributes(std::move(modifiers));
 
+			castle->set_garrisoned(true); // 强制向地图服务器更新城堡坐标。
 			castle->set_coord(new_castle_coord);
 			castle->set_garrisoned(false);
 
