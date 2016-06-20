3acd5707ac9db500b157a71b84a91a3b64a460c2
lhmouse <lh_mouse@126.com>
2016-06-20 14:54:16 +0800

    opt

--- empery_center/src/singletons/world_map.cpp
+++ empery_center/src/singletons/world_map.cpp
@@ -2165,7 +2165,7 @@ boost::shared_ptr<const Poseidon::JobPromise> WorldMap::forced_reload_cluster(Co
 			}	\
 		})
 
-#define ENABLE_PARALLEL_LOADING     0
+#define ENABLE_PARALLEL_LOADING     1
 
 	ASYNC_LOAD_BEGIN {
 		LOG_EMPERY_CENTER_INFO("Loading map cells: scope = ", scope);
