43dd538009ef20ef9f83be97b021f4d9493046cf
lhmouse <lh_mouse@126.com>
2016-08-02 16:33:26 +0800

    opt
    Conflicts:
    	empery_center/src/dungeon_session.cpp
    	empery_center/src/msg/g_packed.hpp

--- empery_center/src/msg/g_packed.hpp
+++ empery_center/src/msg/g_packed.hpp
@@ -25,26 +25,6 @@ namespace Msg {
 	FIELD_STRING        (message)
 #include <poseidon/cbpp/message_generator.hpp>
 
-// 集群到中心单向。只转发，不需要响应。
-#define MESSAGE_NAME    G_PackedAccountNotification
-#define MESSAGE_ID      97
-#define MESSAGE_FIELDS  \
-	FIELD_STRING        (account_uuid)	\
-	FIELD_VUINT         (message_id)	\
-	FIELD_STRING        (payload)
-#include <poseidon/cbpp/message_generator.hpp>
-
-#define MESSAGE_NAME    G_PackedRectangleNotification
-#define MESSAGE_ID      96
-#define MESSAGE_FIELDS  \
-	FIELD_VINT          (x)	\
-	FIELD_VINT          (y)	\
-	FIELD_VUINT         (width)	\
-	FIELD_VUINT         (height)	\
-	FIELD_VUINT         (message_id)	\
-	FIELD_STRING        (payload)
-#include <poseidon/cbpp/message_generator.hpp>
-
 }
 
 }
