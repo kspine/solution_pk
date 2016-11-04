18e6e0175d8d11d3dc3804ea3a1b09e69e921189
root <root@debian>
2016-11-04 18:45:10 +0800

    m3

---
+++ empery_center/src/msg/sc_novice_guide.hpp
@@ -0,0 +1,15 @@
+#ifndef EMPERY_CENTER_MSG_SC_NOVICE_GUIDE_HPP_
+#define EMPERY_CENTER_MSG_SC_NOVICE_GUIDE_HPP_
+
+#include <poseidon/cbpp/message_base.hpp>
+
+namespace EmperyCenter {
+  namespace Msg {
+        #define MESSAGE_NAME SC_GetTaskStepInfoResMessage
+        #define MESSAGE_ID 2002
+        #define MESSAGE_FIELDS \
+                          FIELD_VUINT (task_id) \
+                          FIELD_VUINT (step_id)                                                                                                        #include <poseidon/cbpp/message_generator.hpp>
+  }
+}
+#endif
