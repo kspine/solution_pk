fd55ae0633135260b7ad58cff1963bdeca7d18f5
gengkunpeng <a@example.com>
2016-11-30 10:45:25 +0800

    new task trigger

--- empery_dungeon/src/data/trigger.cpp
+++ empery_dungeon/src/data/trigger.cpp
@@ -38,9 +38,9 @@ namespace {
 	)
 	boost::weak_ptr<const DungeonTriggerContainer> g_dungeon_trigger_container;
 
-	const auto MAX_DUNGE_TRIGGER_COUNT = 27;
+	const auto MAX_DUNGE_TRIGGER_COUNT = 28;
 	const std::string  DUNGEON_TRIGGER_FILES[MAX_DUNGE_TRIGGER_COUNT] = {
-		"dungeon_trigger_0_1","dungeon_trigger_0_2","dungeon_trigger_0_3","dungeon_trigger_0_4","dungeon_trigger_0_5","dungeon_trigger_0_6",
+		"dungeon_trigger_0_1","dungeon_trigger_0_2","dungeon_trigger_0_3","dungeon_trigger_0_4","dungeon_trigger_0_5","dungeon_trigger_0_6","dungeon_trigger_0_7",
 		"dungeon_trigger_1_1","dungeon_trigger_1_2","dungeon_trigger_1_3","dungeon_trigger_1_4","dungeon_trigger_1_5","dungeon_trigger_1_6",
 		"dungeon_trigger_2_1","dungeon_trigger_2_2","dungeon_trigger_2_3","dungeon_trigger_2_4","dungeon_trigger_2_5","dungeon_trigger_2_6",
 		"dungeon_trigger_3_1","dungeon_trigger_3_2","dungeon_trigger_3_3","dungeon_trigger_3_4","dungeon_trigger_3_5","dungeon_trigger_3_6",
