8232919dc524cbe6b72f6a6c69286f7ae3f09119
gengkunpeng <a@example.com>
2016-12-26 13:17:13 +0800

    bug fix

--- empery_center/src/map_cell.cpp
+++ empery_center/src/map_cell.cpp
@@ -178,10 +178,7 @@ void MapCell::pump_production(){
 		const auto occupier_object_uuid = get_occupier_object_uuid();
 		auto parent_object_uuid = get_parent_object_uuid();
 		if(occupier_object_uuid){
-			auto occupier_object = WorldMap::get_map_object(occupier_object_uuid);
-			if(occupier_object){
-				parent_object_uuid = occupier_object->get_parent_object_uuid();
-			}
+			parent_object_uuid = occupier_object_uuid;
 		}
 		auto castle = boost::dynamic_pointer_cast<Castle>(WorldMap::get_map_object(parent_object_uuid));
 		if(!castle){
